ad3dec937855686af724e2f3720b81eeee5a7609
Leyan Buirat <lyanbyrat53@gmail.com>
2025-07-17 10:49:45 +0300

    MVC message

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProductApp.Models;
+namespace ProductApp.Controllers
+{
+    public class ProductController : Controller
+    {
+        // Simulated database
+        private static List<Product> products = new List<Product>();
+
+        public IActionResult Index()
+        {
+            return View(products);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Product product)
+        {
+            products.Add(product);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Details(int id)
+        {
+            var product = products.FirstOrDefault(p => p.Id == id);
+            return View(product);
+        }
+    }
+}
